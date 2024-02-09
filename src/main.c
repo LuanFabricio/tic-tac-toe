2b1ec8ed96f7af4092b7187b1f64bb0c712461b9
LuanFabricio <luanlimaleite@hotmail.com>
2024-02-09 16:43:51 -0300

    feat: Finish x and o draw logic and adding game struct and input handling.

--- src/main.c
+++ src/main.c
@@ -1,79 +1,174 @@
 #include "main.h"
 
 #define LINE_PADDING 15
+#define LINE_HALF_GAP 50
+#define LINE_GAP LINE_HALF_GAP + LINE_HALF_GAP
+#define CIRCLE_RADIUS 42
 
 const Line board_lines[] = {
 	// Horizontal
-	// { .p1 = { .x = BOARD_MIN_X, .y = (HEIGHT/2) - 100 }, .p2 = { .x = BOARD_MAX_X, .y = (HEIGHT/2) - 100 } },
-	{ .p1 = { .x = BOARD_MIN_X, .y = HEIGHT/2 - 50}, .p2 = { .x = BOARD_MAX_X, .y = HEIGHT/2 - 50 } },
-	{ .p1 = { .x = BOARD_MIN_X, .y = HEIGHT/2 + 50}, .p2 = { .x = BOARD_MAX_X, .y = HEIGHT/2 + 50 } },
+	{ .p1 = { .x = BOARD_MIN_X, .y = HEIGHT/2 - LINE_HALF_GAP}, .p2 = { .x = BOARD_MAX_X, .y = HEIGHT/2 - LINE_HALF_GAP } },
+	{ .p1 = { .x = BOARD_MIN_X, .y = HEIGHT/2 + LINE_HALF_GAP}, .p2 = { .x = BOARD_MAX_X, .y = HEIGHT/2 + LINE_HALF_GAP } },
 	// Vertical
-	//{ .p1 = { .x = WIDTH/2 - 100, .y = BOARD_MIN_Y }, .p2 = { .x = WIDTH/2 - 100, .y = BOARD_MAX_Y } },
-	{ .p1 = { .x = WIDTH/2 - 50, .y = BOARD_MIN_Y }, .p2 = { .x = WIDTH/2 - 50, .y = BOARD_MAX_Y } },
-	{ .p1 = { .x = WIDTH/2 + 50, .y = BOARD_MIN_Y }, .p2 = { .x = WIDTH/2 + 50, .y = BOARD_MAX_Y } },
-	// Borders
-	{ .p1 = { .x = BOARD_MIN_X, .y = BOARD_MIN_Y }, .p2 = { .x = BOARD_MAX_X, .y = BOARD_MIN_Y } },
-	{ .p1 = { .x = BOARD_MAX_X, .y = BOARD_MIN_Y }, .p2 = { .x = BOARD_MAX_X, .y = BOARD_MAX_Y } },
-	{ .p1 = { .x = BOARD_MAX_X, .y = BOARD_MAX_Y }, .p2 = { .x = BOARD_MIN_X, .y = BOARD_MAX_Y } },
-	{ .p1 = { .x = BOARD_MIN_X, .y = BOARD_MAX_Y }, .p2 = { .x = BOARD_MIN_X, .y = BOARD_MIN_Y } },
+	{ .p1 = { .x = WIDTH/2 - LINE_HALF_GAP, .y = BOARD_MIN_Y }, .p2 = { .x = WIDTH/2 - LINE_HALF_GAP, .y = BOARD_MAX_Y } },
+	{ .p1 = { .x = WIDTH/2 + LINE_HALF_GAP, .y = BOARD_MIN_Y }, .p2 = { .x = WIDTH/2 + LINE_HALF_GAP, .y = BOARD_MAX_Y } },
 };
 
 static int x = 0;
 const int LINE_COLOR = 0xff1111ff;
 const int BACKGROUND_COLOR = 0xff000000;
 
-#define BOARD_SIDE 3
-#define BOARD_SIZE BOARD_SIDE * BOARD_SIDE
-const char board[BOARD_SIZE] = {0};
+static Game game = {0};
 
-void draw_x(int idx)
+// TODO: Write game logic (win or draw)
+
+int is_valid_board_pos(Vector2 point)
+{
+	int x_valid = ((int)point.x >= BOARD_MIN_X) && ((int)point.x < BOARD_MAX_X);
+	int y_valid = ((int)point.y >= BOARD_MIN_Y) && ((int)point.y < BOARD_MAX_Y);
+
+	return x_valid && y_valid;
+}
+
+Vector2i global_to_board(Vector2 global_point)
+{
+	Vector2i board_point = {0};
+	board_point.x = global_point.x;
+	board_point.y = global_point.y;
+
+	board_point.x -= BOARD_MIN_X;
+	board_point.y -= BOARD_MIN_Y;
+	board_point.x /= LINE_GAP;
+	board_point.y /= LINE_GAP;
+
+	return board_point;
+}
+
+int mouse_to_board(Vector2 mouse)
+{
+
+	if (is_valid_board_pos(mouse)) {
+		Vector2i bp = global_to_board(mouse);
+
+		return bp.y * BOARD_SIDE +  bp.x;
+	}
+
+	return -1;
+}
+
+void handle_click(Vector2 mouse)
+{
+	int board_idx = mouse_to_board(mouse);
+	if (board_idx == -1 || game.board[board_idx] != -1) {
+		return;
+	}
+
+	if (game.is_x_turn) {
+		game.board[board_idx] = 0;
+	} else {
+		game.board[board_idx] = 1;
+	}
+
+	game.is_x_turn = !game.is_x_turn;
+}
+
+void draw_x(int idx, int color)
 {
 	int y = idx / BOARD_SIDE;
 	int x = idx % BOARD_SIDE;
 
-	draw_line(BOARD_MIN_X+LINE_PADDING+x*100    , BOARD_MIN_Y+LINE_PADDING+y*100, BOARD_MIN_X-LINE_PADDING+x*100+100, BOARD_MIN_Y-LINE_PADDING+y*100+100, 0xffffffff);
-	draw_line(BOARD_MIN_X-LINE_PADDING+x*100+100, BOARD_MIN_Y+LINE_PADDING+y*100, BOARD_MIN_X+LINE_PADDING+x*100    , BOARD_MIN_Y-LINE_PADDING+y*100+100, 0xffffffff);
+	draw_line(
+		BOARD_MIN_X+LINE_PADDING+x*100,
+		BOARD_MIN_Y+LINE_PADDING+y*100,
+		BOARD_MIN_X-LINE_PADDING+x*100+100,
+		BOARD_MIN_Y-LINE_PADDING+y*100+100,
+		color);
+	draw_line(
+		BOARD_MIN_X-LINE_PADDING+x*100+100,
+		BOARD_MIN_Y+LINE_PADDING+y*100,
+		BOARD_MIN_X+LINE_PADDING+x*100,
+		BOARD_MIN_Y-LINE_PADDING+y*100+100,
+		color);
 }
 
-void draw_loop() {
-	float dt = get_delta_time();
+void draw_o(int idx, int color)
+{
+	int y = idx / BOARD_SIDE;
+	int x = idx % BOARD_SIDE;
+
+	draw_circle(
+		BOARD_MIN_X+x*100+50,
+		BOARD_MIN_Y+y*100+50,
+		CIRCLE_RADIUS, color);
+}
+
+Vector2 get_mouse_pos()
+{
 	float mouse_x = get_mouse_pos_x();
 	float mouse_y = get_mouse_pos_y();
-	float speed = 500 * dt;
-	if (is_key_pressed(KEY_RIGHT)) {
-		x = min(500, x+speed);
-	}
-	if (is_key_pressed(KEY_LEFT)) {
-		x = max(0, x-speed);
+
+	return  (Vector2){ .x = mouse_x, .y = mouse_y };
+}
+
+void game_loop() {
+	Vector2 mouse = get_mouse_pos();
+	int board_index = mouse_to_board(mouse);
+
+	if (is_mouse_clicked()) {
+		handle_click(mouse);
 	}
 
 	begin_draw();
 
 	clear_window(BACKGROUND_COLOR);
 
-	draw_x(4);
-
-	// draw_rectangle(x, 2, 300, 400, 0xffff0000);
-
-	draw_rectangle(mouse_x-25, mouse_y-25, 50, 50, 0xffffffff);
+	if (board_index >= 0 && game.board[board_index] == -1) {
+		if (game.is_x_turn) {
+			draw_x(board_index, 0xff88ff88);
+		} else {
+			draw_o(board_index, 0xff88ff88);
+		}
+	}
 
 	for (int i = 0; i < 4; i++) {
 		Line l = board_lines[i];
 		draw_line(l.p1.x, l.p1.y, l.p2.x, l.p2.y, LINE_COLOR);
 	}
 
+	for (int i = 0; i < BOARD_SIZE; i++) {
+		char current = game.board[i];
+
+		switch (current) {
+			case 0:
+				draw_x(i, 0xffffffff);
+				break;
+			case 1:
+				draw_o(i, 0xffffffff);
+				break;
+		}
+	}
+
 	end_draw();
+}
+
+void init_game()
+{
+	for (int i = 0; i < BOARD_SIZE; i++) {
+		game.board[i] = -1;
+	}
+	game.is_x_turn = 1;
 }
 
 int main(void)
 {
 	init_window(800, 600, "Tic-tac-toe");
+	init_game();
 
 #ifdef PLATFORM_WEB
-	set_game_loop_func(draw_loop);
+	set_game_loop_func(game_loop);
 #else
 	while(!should_close()) {
-		draw_loop();
+		game_loop();
 	}
 #endif // PLATFORM_WEB
 
--- src/main.h
+++ src/main.h
@@ -14,6 +14,9 @@
 #define BOARD_MIN_Y HEIGHT/2-CENTER_PADDING
 #define BOARD_MAX_Y HEIGHT/2+CENTER_PADDING
 
+#define BOARD_SIDE 3
+#define BOARD_SIZE BOARD_SIDE * BOARD_SIDE
+
 // UTILS
 static int max(int x1, int x2)
 {
@@ -36,20 +39,32 @@ typedef struct {
 
 typedef struct {
 	float x, y;
-} Vector2;
+}Vector2;
+
+typedef struct {
+	int x, y;
+} Vector2i;
+
+typedef struct {
+	char board[BOARD_SIZE];
+	char is_x_turn;
+} Game;
 
 // EXTERNAL INTEFACES
 extern void init_window(int width, int height, const char* title);
 extern int should_close();
 extern void draw_rectangle(int x, int y, int width, int height, int color);
 extern void draw_line(int x0, int y0, int x1, int y1, int color);
+extern void draw_circle(int center_x, int center_y, float radius, int color);
 extern void set_game_loop_func(void (*func));
 extern void clear_window(int color);
 extern void begin_draw();
 extern void end_draw();
 extern int is_key_pressed(char key);
+extern int is_mouse_clicked();
 extern float get_delta_time();
 extern float get_mouse_pos_x();
 extern float get_mouse_pos_y();
+extern void game_log (const char *__restrict __format, ...);
 
 #endif // MAIN_H_
