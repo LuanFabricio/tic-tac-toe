0b004fe326a2dc761b365898fbedde6f860990d7
LuanFabricio <luanlimaleite@hotmail.com>
2024-02-08 23:11:58 -0300

    fix: Fixing amount of lines in the game and introducing draw_x function

--- src/main.c
+++ src/main.c
@@ -1,19 +1,39 @@
 #include "main.h"
 
+#define LINE_PADDING 15
+
 const Line board_lines[] = {
 	// Horizontal
-	{ .p1 = { .x = BOARD_MIN_X, .y = (HEIGHT/2) - 100 }, .p2 = { .x = BOARD_MAX_X, .y = (HEIGHT/2) - 100 } },
-	{ .p1 = { .x = BOARD_MIN_X, .y = HEIGHT/2 }, 	     .p2 = { .x = BOARD_MAX_X, .y = HEIGHT/2 } 	       },
-	{ .p1 = { .x = BOARD_MIN_X, .y = (HEIGHT/2) + 100},  .p2 = { .x = BOARD_MAX_X, .y = (HEIGHT/2) + 100 } },
+	// { .p1 = { .x = BOARD_MIN_X, .y = (HEIGHT/2) - 100 }, .p2 = { .x = BOARD_MAX_X, .y = (HEIGHT/2) - 100 } },
+	{ .p1 = { .x = BOARD_MIN_X, .y = HEIGHT/2 - 50}, .p2 = { .x = BOARD_MAX_X, .y = HEIGHT/2 - 50 } },
+	{ .p1 = { .x = BOARD_MIN_X, .y = HEIGHT/2 + 50}, .p2 = { .x = BOARD_MAX_X, .y = HEIGHT/2 + 50 } },
 	// Vertical
-	{ .p1 = { .x = WIDTH/2 - 100, .y = BOARD_MIN_Y }, .p2 = { .x = WIDTH/2 - 100, .y = BOARD_MAX_Y } },
-	{ .p1 = { .x = WIDTH/2, .y = BOARD_MIN_Y }, 	  .p2 = { .x = WIDTH/2, .y = BOARD_MAX_Y } 	 },
-	{ .p1 = { .x = WIDTH/2 + 100, .y = BOARD_MIN_Y }, .p2 = { .x = WIDTH/2 + 100, .y = BOARD_MAX_Y } },
+	//{ .p1 = { .x = WIDTH/2 - 100, .y = BOARD_MIN_Y }, .p2 = { .x = WIDTH/2 - 100, .y = BOARD_MAX_Y } },
+	{ .p1 = { .x = WIDTH/2 - 50, .y = BOARD_MIN_Y }, .p2 = { .x = WIDTH/2 - 50, .y = BOARD_MAX_Y } },
+	{ .p1 = { .x = WIDTH/2 + 50, .y = BOARD_MIN_Y }, .p2 = { .x = WIDTH/2 + 50, .y = BOARD_MAX_Y } },
+	// Borders
+	{ .p1 = { .x = BOARD_MIN_X, .y = BOARD_MIN_Y }, .p2 = { .x = BOARD_MAX_X, .y = BOARD_MIN_Y } },
+	{ .p1 = { .x = BOARD_MAX_X, .y = BOARD_MIN_Y }, .p2 = { .x = BOARD_MAX_X, .y = BOARD_MAX_Y } },
+	{ .p1 = { .x = BOARD_MAX_X, .y = BOARD_MAX_Y }, .p2 = { .x = BOARD_MIN_X, .y = BOARD_MAX_Y } },
+	{ .p1 = { .x = BOARD_MIN_X, .y = BOARD_MAX_Y }, .p2 = { .x = BOARD_MIN_X, .y = BOARD_MIN_Y } },
 };
 
 static int x = 0;
 const int LINE_COLOR = 0xff1111ff;
 const int BACKGROUND_COLOR = 0xff000000;
+
+#define BOARD_SIDE 3
+#define BOARD_SIZE BOARD_SIDE * BOARD_SIDE
+const char board[BOARD_SIZE] = {0};
+
+void draw_x(int idx)
+{
+	int y = idx / BOARD_SIDE;
+	int x = idx % BOARD_SIDE;
+
+	draw_line(BOARD_MIN_X+LINE_PADDING+x*100    , BOARD_MIN_Y+LINE_PADDING+y*100, BOARD_MIN_X-LINE_PADDING+x*100+100, BOARD_MIN_Y-LINE_PADDING+y*100+100, 0xffffffff);
+	draw_line(BOARD_MIN_X-LINE_PADDING+x*100+100, BOARD_MIN_Y+LINE_PADDING+y*100, BOARD_MIN_X+LINE_PADDING+x*100    , BOARD_MIN_Y-LINE_PADDING+y*100+100, 0xffffffff);
+}
 
 void draw_loop() {
 	float dt = get_delta_time();
@@ -31,11 +51,13 @@ void draw_loop() {
 
 	clear_window(BACKGROUND_COLOR);
 
+	draw_x(4);
+
 	// draw_rectangle(x, 2, 300, 400, 0xffff0000);
 
 	draw_rectangle(mouse_x-25, mouse_y-25, 50, 50, 0xffffffff);
 
-	for (int i = 0; i < 6; i++) {
+	for (int i = 0; i < 4; i++) {
 		Line l = board_lines[i];
 		draw_line(l.p1.x, l.p1.y, l.p2.x, l.p2.y, LINE_COLOR);
 	}
